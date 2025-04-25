0e8d782f956f8a052bdfd89421eac88465adf56c
Axel-Zaragoci <axelzaragoci@gmail.com>
2025-04-25 17:09:06 +0200

    Creo un constructor para la clase libro

--- proyecto_libros/src/proyecto_libros/Libro.java
+++ proyecto_libros/src/proyecto_libros/Libro.java
@@ -7,4 +7,13 @@ public class Libro {
 	private String saga;
 	private int paginas;
 	private int publicacion;
+	
+	public Libro(String titulo, String editorial, String autor, String saga, int paginas, int publicacion) {
+		this.titulo = titulo;
+		this.editorial = editorial;
+		this.autor = autor;
+		this.saga = saga;
+		this.paginas = paginas;
+		this.publicacion = publicacion;
+	}
 }
