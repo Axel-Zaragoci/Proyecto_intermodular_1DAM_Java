cace49a571aa7b64d7b32c3ecdff90151c798e8f
Axel-Zaragoci <axelzaragoci@gmail.com>
2025-04-25 17:07:57 +0200

    Creo las variables de instancia de la clase libro

--- proyecto_libros/src/proyecto_libros/Libro.java
+++ proyecto_libros/src/proyecto_libros/Libro.java
@@ -1,5 +1,10 @@
 package proyecto_libros;
 
 public class Libro {
-
+	private String titulo;
+	private String editorial;
+	private String autor;
+	private String saga;
+	private int paginas;
+	private int publicacion;
 }
