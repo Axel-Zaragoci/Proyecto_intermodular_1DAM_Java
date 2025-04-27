304cf97782ed98c1601b5397686249bbfce463a3
Axel-Zaragoci <axelzaragoci@gmail.com>
2025-04-27 18:02:47 +0200

    Creo las variables para la clase

--- proyecto_libros/src/proyecto_libros/Editorial.java
+++ proyecto_libros/src/proyecto_libros/Editorial.java
@@ -1,5 +1,10 @@
 package proyecto_libros;
 
 public class Editorial {
-
+	private String nombre;
+	private String pais;
+	private String ciudad;
+	private int fundacion;
+	private long telefono;
+	private String email;
 }
