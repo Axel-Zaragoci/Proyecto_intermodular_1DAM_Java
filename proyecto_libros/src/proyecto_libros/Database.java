9077f4010d21ef09dc0003e18b5bce189e8a415d
Axel-Zaragoci <axelzaragoci@gmail.com>
2025-04-27 23:32:32 +0200

    Creo un método para realizar la conexión con la base de datos

--- proyecto_libros/src/proyecto_libros/Database.java
+++ proyecto_libros/src/proyecto_libros/Database.java
@@ -1,5 +1,20 @@
 package proyecto_libros;
 
-public class Database {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class Database {
+	private static String url = "jdbc:postgresql://localhost:5432/Proyecto";
+    private static String usuario = "postgres";
+    private static String contraseña = "1234";
+    
+    public static Connection conectar() {
+    	try {
+			return DriverManager.getConnection(url, usuario, contraseña);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+    }
 }
