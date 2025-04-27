3dd6b21902aec898ff4d436c80bc2660e2f94652
Axel-Zaragoci <axelzaragoci@gmail.com>
2025-04-27 18:32:15 +0200

    Creo las variables de la clase Autor

--- proyecto_libros/src/proyecto_libros/Autor.java
+++ proyecto_libros/src/proyecto_libros/Autor.java
@@ -1,5 +1,9 @@
 package proyecto_libros;
 
 public class Autor {
-
+	private String nombre;
+	private String fechaNacimiento;
+	private String nacionalidad;
+	private boolean vivo;
+	private String seudonimo;
 }
