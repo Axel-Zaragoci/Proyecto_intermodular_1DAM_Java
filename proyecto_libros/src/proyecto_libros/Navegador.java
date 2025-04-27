2c76e2ae7d1a5322a370e82d78d5c0b022e03e80
Axel-Zaragoci <axelzaragoci@gmail.com>
2025-04-27 19:00:17 +0200

    Creo los métodos para mostrar mensajes de confirmación, error y confirmación en la clase Navegador

--- proyecto_libros/src/proyecto_libros/Navegador.java
+++ proyecto_libros/src/proyecto_libros/Navegador.java
@@ -3,6 +3,7 @@ package proyecto_libros;
 import java.util.ArrayList;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class Navegador {
 	private static ArrayList<JFrame> ventanas = new ArrayList<>();
@@ -29,5 +30,17 @@ public class Navegador {
 			return;
 		}
 		System.out.println("Ventana no encontrada");
+	}
+	
+	public static void mostrarMensajeError(JFrame v,String titulo, String mensaje) {
+		JOptionPane.showMessageDialog(v, mensaje, titulo, JOptionPane.ERROR_MESSAGE);
+	}
+	
+	public static int mostrarMensajePregunta(JFrame v, String titulo, String mensaje) {
+		return JOptionPane.showConfirmDialog(v, mensaje, titulo, JOptionPane.YES_NO_OPTION);
+	}
+	
+	public static void mostrarMensajeInformacion(JFrame v, String titulo, String mensaje) {
+		JOptionPane.showMessageDialog(v, mensaje, titulo, JOptionPane.INFORMATION_MESSAGE);
 	}
 }
