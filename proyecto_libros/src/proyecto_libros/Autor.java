8f6b246524a098eb40f966a52fea9664cc2b456d
Axel-Zaragoci <axelzaragoci@gmail.com>
2025-04-27 18:38:40 +0200

    Creo los getters y setters para la clase Autor

--- proyecto_libros/src/proyecto_libros/Autor.java
+++ proyecto_libros/src/proyecto_libros/Autor.java
@@ -6,4 +6,44 @@ public class Autor {
 	private String nacionalidad;
 	private boolean vivo;
 	private String seudonimo;
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public void setFechaNacimiento(String fecha) {
+		this.fechaNacimiento = fecha;
+	}
+	
+	public void setNacionalidad(String nacionalidad) {
+		this.nacionalidad = nacionalidad;
+	}
+	
+	public void setVivo(boolean vivo) {
+		this.vivo = vivo;
+	}
+	
+	public void setSeudonimo(String seudonimo) {
+		this.seudonimo = seudonimo;
+	}
+	
+	public String getNombre() {
+		return this.nombre;
+	}
+	
+	public String getFechaNacimiento() {
+		return this.fechaNacimiento;
+	}
+	
+	public String getNacionalidad() {
+		return this.nacionalidad;
+	}
+	
+	public boolean isVivo() {
+		return this.vivo;
+	}
+	
+	public String getSeudonimo() {
+		return this.seudonimo;
+	}
 }
