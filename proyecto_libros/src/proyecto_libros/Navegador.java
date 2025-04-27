43afcda4a00cb1d858aa08e48d18fce7e9765ef9
Axel-Zaragoci <axelzaragoci@gmail.com>
2025-04-27 18:59:14 +0200

    Creo los métodos para tratar con ventanas en la clase Navegador

--- proyecto_libros/src/proyecto_libros/Navegador.java
+++ proyecto_libros/src/proyecto_libros/Navegador.java
@@ -6,4 +6,28 @@ import javax.swing.JFrame;
 
 public class Navegador {
 	private static ArrayList<JFrame> ventanas = new ArrayList<>();
+	
+	public static void argegarVentanas(JFrame v) {
+		if(!ventanas.contains(v)) {
+			ventanas.add(v);
+		}
+	}
+	
+	public static JFrame obtenerVentana(String titulo) {
+		for (JFrame v : ventanas) {
+			if (v.getTitle().equalsIgnoreCase(titulo)) {
+				return v;
+			}
+		}
+		return null;
+	}
+	
+	public static void dispatcher(String titulo, boolean visibilidad) {
+		JFrame v = obtenerVentana(titulo);
+		if (v != null) {
+			v.setVisible(visibilidad);
+			return;
+		}
+		System.out.println("Ventana no encontrada");
+	}
 }
