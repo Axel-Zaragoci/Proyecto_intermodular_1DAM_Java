84418c18f447c3ca22bc3f632e2ddd448b30bc59
Axel-Zaragoci <axelzaragoci@gmail.com>
2025-04-30 17:27:53 +0200

    Creo un método que me permita insertar autores a la base de datos

--- proyecto_libros/src/proyecto_libros/AutorController.java
+++ proyecto_libros/src/proyecto_libros/AutorController.java
@@ -1,5 +1,29 @@
 package proyecto_libros;
 
-public class AutorController {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+public class AutorController {	
+	
+	public static void crear(Autor autor) {
+		String sql = "INSERT INTO autor (nombre, fecha_nacimiento, nacionalidad, estado, seudonimo) VALUES (?, ?, ?, ?, ?);";
+		try (Connection con = Database.conectar();
+			 PreparedStatement stmt = con.prepareStatement(sql)) {
+			
+			stmt.setString(1, autor.getNombre());
+			stmt.setDate(2, java.sql.Date.valueOf("1964-06-05"));
+			stmt.setString(3, autor.getNacionalidad());
+			stmt.setString(4, autor.isVivo()?"Vivo":"Fallecido");
+			stmt.setString(5, autor.getSeudonimo());
+			
+			stmt.execute();
+			System.out.println("Autor añadido");
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
 }
