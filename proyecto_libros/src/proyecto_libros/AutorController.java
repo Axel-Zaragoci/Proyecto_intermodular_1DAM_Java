4fae381de944deaafec3965c186ba9b340da44c1
Axel-Zaragoci <axelzaragoci@gmail.com>
2025-04-30 17:45:25 +0200

    Creo un método para obtener todos los autores de la base de datos

--- proyecto_libros/src/proyecto_libros/AutorController.java
+++ proyecto_libros/src/proyecto_libros/AutorController.java
@@ -2,7 +2,10 @@ package proyecto_libros;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 public class AutorController {	
 	
@@ -23,6 +26,32 @@ public class AutorController {
 		catch (SQLException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static ArrayList<Autor> ver() {
+		ArrayList<Autor> lista = new ArrayList<>();
+		String sql = "SELECT * FROM autor";
+		
+		try (Connection con = Database.conectar();
+			 Statement stmt = con.createStatement();
+			 ResultSet rs = stmt.executeQuery(sql)) {
+			
+			while (rs.next()) {
+				boolean vivo;
+				if (rs.getString("estado") != null && (rs.getString("estado").equalsIgnoreCase("Vivo") || rs.getString("estado").equalsIgnoreCase("Viva"))) {
+					vivo = true;
+				}
+				else {
+					vivo = false;
+				}
+				Autor autor = new Autor(rs.getString("nombre"), rs.getString("fecha_nacimiento"), rs.getString("nacionalidad"), vivo, rs.getString("seudonimo"));
+				lista.add(autor);
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return lista;
 	}
 	
 	
