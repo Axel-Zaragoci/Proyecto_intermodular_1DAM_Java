1bfd74d682d2219360bc08f3192cdfb29a0c1a2a
Axel-Zaragoci <axelzaragoci@gmail.com>
2025-05-03 18:10:18 +0200

    Modifico la clase Libro.java para que se adapte a los cambios en la base de datos

--- proyecto_libros/src/proyecto_libros/Libro.java
+++ proyecto_libros/src/proyecto_libros/Libro.java
@@ -1,23 +1,25 @@
 package proyecto_libros;
 
 import java.util.ArrayList;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class Libro {
 	private String titulo;
-	private ArrayList<String> editorial = new ArrayList<>();
-	private ArrayList<String> autor = new ArrayList<>();
+	private int editorial;
+	private ArrayList<Integer> autor = new ArrayList<>();
 	private int paginas;
 	private int publicacion;
 	private double precio;
 	private long isbn;
 	private String idioma;
 	
-	public Libro(String titulo, String[] editorial, String[] autor, String saga, int paginas, int publicacion, double precio, long isbn, String idioma) {
+	public Libro(String titulo, int editorial, Integer[] autor, String saga, int paginas, int publicacion, double precio, long isbn, String idioma) {
 		this.titulo = titulo;
-		for(String e : editorial) {
-			this.editorial.add(e);
-		}
-		for(String a : autor) {
+		this.editorial = editorial;
+		for(Integer a : autor) {
 			this.autor.add(a);
 		}
 		this.paginas = paginas;
@@ -25,53 +27,5 @@ public class Libro {
 		this.precio = precio;
 		this.isbn = isbn;
 		this.idioma = idioma;
-	}
-	
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-	
-	public void setPaginas(int paginas) {
-		this.paginas = paginas;
-	}
-	
-	public void setPublicacion(int publicacion) {
-		this.publicacion = publicacion;
-	}
-	
-	public void setPrecio(double precio) {
-		this.precio = precio;
-	}
-	
-	public void setIsbn(long isbn) {
-		this.isbn = isbn;
-	}
-	
-	public void setIdioma(String idioma) {
-		this.idioma = idioma;
-	}
-	
-	public String getTitulo() {
-		return this.titulo;
-	}
-	
-	public int getPaginas() {
-		return this.paginas;
-	}
-	
-	public int getPublicacion() {
-		return this.publicacion;
-	}
-	
-	public double getPrecio() {
-		return this.precio;
-	}
-	
-	public long getIsbn() {
-		return this.isbn;
-	}
-	
-	public String getIdioma() {
-		return this.idioma;
 	}
 }
