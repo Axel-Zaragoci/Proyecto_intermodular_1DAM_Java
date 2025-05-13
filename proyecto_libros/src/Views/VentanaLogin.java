86d8cdb2f6b76a5bd59e99251dd635febc27008c
Axel-Zaragoci <axelzaragoci@gmail.com>
2025-05-13 15:45:02 +0200

    Cambio el campo de contraseña

--- proyecto_libros/src/Views/VentanaLogin.java
+++ proyecto_libros/src/Views/VentanaLogin.java
@@ -1,21 +1,21 @@
 package Views;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
 import Config.config;
 import Controllers.Navegador;
 
-import javax.swing.JLabel;
-import javax.swing.JTextField;
-import javax.swing.JButton;
-
 public class VentanaLogin extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField userTextField;
-	private JTextField passwdTextField;
+	private JPasswordField passwdTextField;
 
 	public VentanaLogin() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -49,30 +49,35 @@ public class VentanaLogin extends JFrame {
 		passwdLabel.setBounds(10, 107, 130, 14);
 		panel.add(passwdLabel);
 		
-		passwdTextField = new JTextField();
+		passwdTextField = new JPasswordField();
 		passwdTextField.setBounds(10, 121, 204, 20);
 		panel.add(passwdTextField);
 		passwdTextField.setColumns(10);
 		
 		JButton loginButton = new JButton("Iniciar sesión");
 		loginButton.addActionListener( e -> {
-			if (userTextField.getText().trim().isBlank() || userTextField.getText().trim().isEmpty() || userTextField.getText().trim().equals("")) {
-				Navegador.mostrarMensajeError(VentanaLogin.this, "Error", "Debes indicar un usuario");
-				return;
-			}
-			if (passwdTextField.getText().trim().isBlank() || passwdTextField.getText().trim().isEmpty() || passwdTextField.getText().trim().equals("")) {
-				Navegador.mostrarMensajeError(VentanaLogin.this, "Error", "Debes indicar una contraseña");
-				return;
-			}
-			if (userTextField.getText().trim().equalsIgnoreCase(config.getUsuario()) && passwdTextField.getText().trim().equalsIgnoreCase(config.getContraseña())) {
-				Navegador.agregarVentanas(new VentanaPrincipal());
-				Navegador.dispatcher("Menu", true);
-				Navegador.dispatcher(getTitle(), false);
-				return;
-			}
-			Navegador.mostrarMensajeError(VentanaLogin.this, "Error", "Datos incorrectos");
+			revision();
 		});
 		loginButton.setBounds(10, 174, 130, 23);
 		panel.add(loginButton);
+	}
+	
+	void revision() {
+		if (userTextField.getText().trim().isBlank() || userTextField.getText().trim().isEmpty() || userTextField.getText().trim().equals("")) {
+			Navegador.mostrarMensajeError(VentanaLogin.this, "Error", "Debes indicar un usuario");
+			return;
+		}
+		if (passwdTextField.getPassword().length == 0) {
+			Navegador.mostrarMensajeError(VentanaLogin.this, "Error", "Debes indicar una contraseña");
+			return;
+		}
+		String passwd = String.valueOf(passwdTextField.getPassword());
+		if (userTextField.getText().trim().equalsIgnoreCase(config.getUsuario()) && passwd.equals(config.getContraseña())) {
+			Navegador.agregarVentanas(new VentanaPrincipal());
+			Navegador.dispatcher("Menu", true);
+			Navegador.dispatcher(getTitle(), false);
+			return;
+		}
+		Navegador.mostrarMensajeError(VentanaLogin.this, "Error", "Datos incorrectos");
 	}
 }
